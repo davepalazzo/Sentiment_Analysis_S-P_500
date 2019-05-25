7064d75f62526bb608f8521c30c90c8e54b372ee
Dave Palazzo <david.palazzo4@du.edu>
2019-05-25 11:51:40 -0600

    Updates

--- cleanMDA.py
+++ cleanMDA.py
@@ -3,12 +3,13 @@ from bs4 import BeautifulSoup as bsoup
 from collections import defaultdict
 
 
-def pullMDA(yearRange,compName, resLst):
+def pullMDA(yearRange,compName, resLst, split_on = None):
     '''Pulls MD&A text from exhibit 13 SEC website
         Parameters: 1) yearRange - list of years
                     2) resLst - list of SEC websites
                     3) compName - string of company name
-        Returns: Tuple with year and MD&A text
+                    4) split_on - string to split on for MD&A
+        Returns: Tuple with year and MD&A text 
         '''
     output = defaultdict()
     for year in yearRange:
@@ -16,9 +17,14 @@ def pullMDA(yearRange,compName, resLst):
             res = requests.get(site)
             soup = bsoup(res.content,'html.parser')
             text = soup.text
-            output[year] = (compName,text)
-            break
-    return output
+            if split_on:
+                text = text.split(split_on)[1]
+                output[year] = (compName,text)
+                break
+            else:    
+                output[year] = (compName,text)
+                break
+    return output 
 
 
 def divide_chunks(Lst, n): 
